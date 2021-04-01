37eb2e3d0d9896f66a2dc06f1a0b7d32c9a91c4e
Walther Lee <walthere.lee@gmail.com>
2021-04-01 15:22:21 -0700

    add getUser to ClientSuggestic

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -2,17 +2,29 @@ import { GraphQLClient } from 'graphql-request';
 import { getSdk, Sdk } from './__generated_sdk';
 
 class ClientSuggestic extends GraphQLClient {
-  constructor(token: string, user: string) {
-    const url = 'https://production.suggestic.com/graphql';
+  private token: string
+  private sourceURL: string
+
+  constructor(token: string, user?: string, url?: string) {
+    url = url || 'https://production.suggestic.com/graphql'; 
     const options = {
       headers: {
         Authorization: `Token ${token}`,
-        'SG-User': user,
       },
     };
+    if (!!user) {
+      options.headers['SG-User'] = user;
+    }
+
     super(url, options);
 
+    this.token = token;
+    this.sourceURL = url;
     Object.assign(this, getSdk(this));
+  }
+
+  getUser(userId: string): ClientSuggestic {
+    return new ClientSuggestic(this.token, userId, this.sourceURL);
   }
 }
 
