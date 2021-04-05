4f0cd0f34d470ee84966a77ce3a349fc5ed8c841
Ernesto Vargas <me@netoxico.com>
2021-04-05 10:39:25 -0500

    prettify utils

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -4,10 +4,9 @@
  * @param seed user agent properties to serialize
  * @returns the serialized user agent string
  */
- export function serializeUserAgent(seed: Record<string, string>): string {
-    return Object.entries(seed).reduce((acc, [key, value]) => {
-      const encoded = `${key}@${encodeURIComponent(value)}`;
-      return acc ? `${acc} ${encoded}` : encoded;
-    }, "");
-  }
-  
+export function serializeUserAgent(seed: Record<string, string>): string {
+  return Object.entries(seed).reduce((acc, [key, value]) => {
+    const encoded = `${key}@${encodeURIComponent(value)}`;
+    return acc ? `${acc} ${encoded}` : encoded;
+  }, '');
+}
